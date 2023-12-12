4df13c0f9ccfa6b1b62a2c2ecc7160ec513c4b67
Miguel Raposo <migpovrap@hotmail.com>
2023-12-12 10:24:37 +0000

    First Commit, inclui o ficheiro auxiliar com os puzles e o Projeto

---
+++ Projeto.pl
@@ -0,0 +1,5 @@
+%Miguel Póvoa Raposo, ist1109686
+:- use_module(library(clpfd)). % para poder usar transpose/2
+:- set_prolog_flag(answer_write_options,[max_depth(0)]). % ver listas completas
+:- ['puzzlesAcampar.pl']. % Ficheiro dado. No Mooshak tera mais puzzles.
+
